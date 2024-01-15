5bfbcb7edfc65d57886023ed1e13e214221ad0fc
sakar <sakarkd@gmail.com>
2024-01-15 19:06:01 +0545

    make jarvis speakable

--- jarvis.py
+++ jarvis.py
@@ -1,3 +1,20 @@
 import pyttsx3
 
-engine = pyttsx3.init()
+engine = pyttsx3.init() #engine is variable and init is a function
+
+voices = engine.getProperty('voices') # voices is also a variable 
+
+engine.setProperty('voice',voices[1].id)
+
+engine.setProperty('rate',150) #rate of speed is 200
+
+
+# engine.say('hello world vandeeu') #makes jarvis speak
+# engine.runAndWait()
+
+def talk(text): #talk is a fn
+    engine.say(text) 
+    engine.runAndWait()
+talk('hello world vandeeu')
+
+ 
